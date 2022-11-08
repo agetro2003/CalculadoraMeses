9b9f03a2ae70e7bbab91617e2c7dc2d96d906ce9
Jesús Ortega <jesusdaniolob@gmail.com>
2022-11-08 12:28:03 -0400

    Añadir calculadora de dolares

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function calcular(){
     var meses = document.getElementById("meses").value;
+    var dolares = document.getElementById("dolares").value
     var resultado = document.getElementById("resultado");
 let resta = meses * 1;
 let total = 0
@@ -13,7 +14,11 @@ console.log(total);
 resta --;
 
 }
-resultado.innerHTML = `<h1> El resultado es ${total} </h1>`
+
+let multiplicacion = dolares * total * 1.25
+resultado.innerHTML = `<h1> El resultado es ${total} </h1>
+<h1> ${total} x ${dolares} x 1.25 = ${multiplicacion} </h1>`
+
 
 }
 
